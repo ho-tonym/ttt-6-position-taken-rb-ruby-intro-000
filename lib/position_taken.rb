ae2ba512b311d0d1ae0e70e6a23fd9bce85e0b18
Tony Ho <ho.tonym@gmail.com>
2018-04-24 01:30:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,7 +7,7 @@ def position_taken(board, index)
       return false
   elsif board[index] == nil
       return false
-  elsif board[index] == "X"||"O"
+  elsif board[index] == "X"|| board[index] == "O"
       return true
   end
 end
