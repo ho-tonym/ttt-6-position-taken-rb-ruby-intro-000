b8346995f887b03764dc1d3db868e1bec64e5020
Tony Ho <ho.tonym@gmail.com>
2018-04-24 01:05:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,13 @@
-def position_taken(board, index_number)
+def position_taken(board, index)
 
+if board[index] == " "
+  return false
 
+elsif board[index] == ""
+    return false
+elsif board[index] == nil
+      return false
+elsif board[index] == "X"||"O"
+          return true
 
 end
